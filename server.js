b30ab5cdfb02932b4845720889a39d9ad3baa107
anzbert <38823700+anzbert@users.noreply.github.com>
2021-08-03 07:40:02 +0800

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/webcam*", (req, res) => {
-  res.redirect("http://192.168.1.202:8081")
+  res.redirect("http://127.0.0.1:8081")
 });
 
 app.use(express.static(PUBLIC_FOLDER));
