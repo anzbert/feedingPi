f724bac3e714720dfa41a511269dfd69ba699a7e
anzbert <38823700+anzbert@users.noreply.github.com>
2021-08-04 16:04:01 +0800

    .

--- public/client.js
+++ public/client.js
@@ -16,22 +16,3 @@ feedButtons.forEach((button, number) =>{
   });
 
 })
-
-
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
-
-button.addEventListener("click", () => {
-  fetch("/button1", { method: "POST" })
-    .then(function (response) {
-      if (response.ok) {
-        console.log("Click was recorded");
-        return;
-      }
-      throw new Error("Request failed.");
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
