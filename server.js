f553123e12cc2b948312dd48837c0a02029a33f3
anzbert <38823700+anzbert@users.noreply.github.com>
2021-08-04 10:44:58 +0800

    .

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const { spawn } = require("child_process");
 try {
-  const mjpgStreamer = spawn("mjpg_streamer ", [
-    "-i",
-    "input_uvc.so",
-    "-o",
-    "output_http.so",
-    "-p",
-    "8080",
-  ]);
+  const mjpgStreamer = spawn("mjpg_streamer");
+  // const mjpgStreamer = spawn("mjpg_streamer", [
+  //   "-i",
+  //   "input_uvc.so",
+  //   "-o",
+  //   "output_http.so",
+  //   "-p",
+  //   "8080",
+  // ]);
 } catch (err) {
   console.log(err);
 }
 
 const process = require("process");
-
 process.on("beforeExit", (code) => {
   mjpgStreamer.kill();
 });
