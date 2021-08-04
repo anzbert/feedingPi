45ab63c2fae2ae577aef9cb415bff95cb1d9c336
anzbert <38823700+anzbert@users.noreply.github.com>
2021-08-04 11:14:19 +0800

    .

--- server.js
+++ server.js
@@ -1,53 +1,71 @@
+// REQUIRE MODULES
+const path = require("path");
+const express = require("express");
 const process = require("process");
 const { spawn } = require("child_process");
-  // const mjpgStreamer = spawn("mjpg_streamer");
-  const mjpgStreamer = spawn("mjpg_streamer", [
-    "-i",
-    "input_uvc.so",
-    "-o",
-    "output_http.so -p 8080"
-  ]);
-  mjpgStreamer.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-  
-  mjpgStreamer.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  
-  mjpgStreamer.on('close', (code) => {
-    console.log(`mjpgStreamer exited with code: ${code}`);
-    process.exit();
-  });
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// DEFINE GLOBAL CONSTANTS:
+const PORT = 3000;
+const PUBLIC_FOLDER = path.join(__dirname, "public");
+
+// LAUNCH AND KILL MJPG STREAMER:
+const mjpgStreamer = spawn("mjpg_streamer", [
+  "-i",
+  "input_uvc.so",
+  "-o",
+  "output_http.so -p 8080",
+]);
+
+mjpgStreamer.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+mjpgStreamer.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
+mjpgStreamer.on("close", (code) => {
+  console.log(`mjpgStreamer exited with code: ${code}`);
+  process.exit();
+});
 
 process.on("beforeExit", (code) => {
   mjpgStreamer.kill();
 });
 
-// dotenv for environment variables??
-const path = require("path");
+// PROXY
+const proxyOptions = {
+  target: '192.168.1.202:8080', // target host
+  // changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  // pathRewrite: {
+  //   '^/api/old-path': '/api/new-path', // rewrite path
+  //   '^/api/remove/path': '/path', // remove base path
+  // },
+  // router: {
+  //   // when request.headers.host == 'dev.localhost:3000',
+  //   // override target 'http://www.example.org' to 'http://localhost:8000'
+  //   'dev.localhost:3000': 'http://localhost:8000',
+  // },
+};
+const proxy = createProxyMiddleware(proxyOptions);
 
-const express = require("express");
+// EXPRESS:
 const app = express();
 
-// CONSTANTS:
-const PORT = 3000;
-const PUBLIC_FOLDER = path.join(__dirname, "public");
-
-// DEFINE MIDDLEWARE:
 app.use((req, res, next) => {
   console.log(`${req.ip} requesting: ${req.url}`);
   next();
 });
 
-app.get("/webcam*", (req, res) => {
-  res.redirect("http://192.168.1.202:8080/?action=stream");
-});
+app.use('/webcam*', proxy);
+// app.get("/webcam*", (req, res) => {
+//   // res.redirect("http://192.168.1.202:8080/?action=stream");
+
+// });
 
 app.use(express.static(PUBLIC_FOLDER));
 
-// START SERVER:
+// START HTTP SERVER:
 app.listen(PORT, () => {
-  console.info(`Listening on Port ${PORT}`);
+  console.info(`NODE SERVER - Listening on Port ${PORT}`);
 });
